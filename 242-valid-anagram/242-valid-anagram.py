87d29d3304641aa87e61941ee8bd658e40a83baf
Nikhil <44422494+Bad33@users.noreply.github.com>
2022-04-06 16:58:45 +0545

    Time: 110 ms (9.14%), Space: 15.7 MB (12.80%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        str_lst,target = sorted(s),sorted(t)
+        s = ''.join([str(item) for item in str_lst])
+        t = ''.join([str(item) for item in target])
+        
+        if s == t:
+            return True
+        else:
+            return False
+        
