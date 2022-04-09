fd2e4831808e54cde6d67a704675a34b0f0230d2
Nikhil <44422494+Bad33@users.noreply.github.com>
2022-04-09 11:20:09 +0545

    Time: 282 ms (20.40%), Space: 17.2 MB (8.29%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def check(lo, hi, removed=False):
+            while lo < hi:
+                if s[lo] != s[hi]:
+                    if removed:
+                        return False
+                    return check(lo+1, hi, True) or check(lo, hi-1, True)
+                lo += 1
+                hi -= 1
+            return True
+        return check(0, len(s)-1)
